ed0c5bc20b56fd670cedeff7845590b957d392bd
AryanRaj2410 <aryanchoudhury24@gmail.com>
2025-07-27 00:51:56 +0530

    updated project section

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -15,5 +15,12 @@ export const tabData = [ {
     "description" : "Built a responsive web-based currency converter using HTML, CSS, and JavaScript.\n◦ Integrated ExchangeRate API to fetch live exchange rates for accurate real-time conversions.\n◦ Designed an intuitive user interface for seamless currency selection and instant results.",
     "link" : "https://github.com/AryanRaj2410/Realtime-currency-converter",
     "tech" : [ "HTML", "CSS", "JavaScript" ]
-  } ]
+  } 
+, {
+    "title" : "Portfolio",
+    "description" : "◦Designed and developed a fully responsive personal portfolio using ReactJS, Javascript, and CSS, showcasing projects, skills, and professional background in a clean, user-centric layout.\n◦Integrated Framer Motion to enhance user experience with smooth, modern animations such as fade-ins, slide transitions, and hover effects for components.\n◦Utilized ReactJS to structure the website with reusable and modular components like Navbar, About, Projects, Contact, and Footer for maintainability and scalability.\n◦Ensured optimal viewing experience across all screen sizes using responsive design principles and thorough testing across major web browsers.",
+    "link" : "https://aryanraj24.netlify.app/",
+    "tech" : [ "JavaScript", "CSS", "ReactJS", "Framer Motion","Vs Code" ]
+  
 } ]
+}]
